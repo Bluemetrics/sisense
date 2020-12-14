7039f6d50ac0891a220e14c347b9a30d6af01dc6
bernardo-trevizan <bernardo.trevizan@bluemetrics.com.br>
2020-12-14 15:48:57 -0300

    Check if string is json parsable

--- sisense/utils.py
+++ sisense/utils.py
@@ -1,5 +1,24 @@
+import json
 import os
 
 
-def is_windows():
+def is_windows() -> bool:
+    """Check if OS is Windows. Otherwise, assume it is linux.
+
+    :return: (bool) True, if OS is Windows. False, otherwise.
+    """
     return os.name == 'nt'
+
+
+def is_json(value: str) -> bool:
+    """Check if string is a JSON parsable.
+
+    :param value: (str) Value to check.
+    :return: (bool) True, if value is a valid JSON string. False, otherwise.
+    """
+    try:
+        _ = json.loads(value)
+    except ValueError:
+        return False
+
+    return True
